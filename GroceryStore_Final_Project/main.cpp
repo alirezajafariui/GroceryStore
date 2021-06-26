a8023ce564202396e1271883585cc7b074453326
alirezajafari <a.r.jafari.ui@gmail.com>
2021-06-26 12:12:41 +0430

    class product

--- GroceryStore_Final_Project/main.cpp
+++ GroceryStore_Final_Project/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 using namespace std ;
 
+class product
+{
+    private :
+        string name ;
+        int num ;
+        product * next ;
+    public :
+        string getName ()
+        {
+            return this->name ;
+        }
+        void setName ( string name )
+        {
+            this->name = name ;
+        }
+
+        int getNum ()
+        {
+            return this->num ;
+        }
+        void setNum ( int num )
+        {
+            this->num = num ;
+        }
+
+        product * getNext ()
+        {
+            return this->next ;
+        }
+        void setNext ( product * next )
+        {
+            this->next = next ;
+        }
+};
+
+
+
 int main( void )
 {
 
