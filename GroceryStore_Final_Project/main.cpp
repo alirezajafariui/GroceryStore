0f376ae6515e57c8d3101cfcc01889946532fb3d
alirezajafari <a.r.jafari.ui@gmail.com>
2021-06-27 11:42:01 +0430

    Addproduct and print functions added.

--- GroceryStore_Final_Project/main.cpp
+++ GroceryStore_Final_Project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <map>
+#include <Qmap>
+#include <QString>
 
 using namespace std ;
 
@@ -15,10 +16,18 @@ class productkind
             product * next ;
             product * prev ;
 
+            product ()
+            {
+                this->available = 0 ;
+            }
             product ( string name , int available )
             {
                 this->name = name ;
-                this->available = available ;
+                this->available += available ;
+            }
+            ~product ()
+            {
+                this->available = 0 ;
             }
         };
 
@@ -42,19 +51,19 @@ class productkind
         ~productkind()
         {
             product * pro = this->head ;
-            
+
             while ( pro != nullptr )
             {
                 product * tmp = pro ;
                 pro = pro->next ;
                 delete [] tmp ;
             }
-            
+
             this->head = nullptr ;
             this->tail = nullptr ;
             this->pknum = 0 ;
         }
-        
+
         void addProduct ( string name , int available )
         {
             product * pro = new product ( name , available ) ;
@@ -72,21 +81,21 @@ class productkind
             {
                 product * tmp = this->head ;
 
-                if ( pro <= this->head )
+                if ( pro->name <= this->head->name )
                 {
                     pro->prev = this->head->prev ;
                     pro->next = this->head ;
                     this->head = pro ;
                     return ;
                 }
-                if ( pro >= this->tail )
+                if ( pro->name >= this->tail->name )
                 {
                     pro->prev = this->tail ;
                     pro->next = this->tail->next ;
                     this->tail = pro ;
                     return ;
                 }
-                if ( pro <= tmp->next )
+                if ( pro->name <= tmp->next->name )
                 {
                     pro->prev = tmp ;
                     pro->next = tmp->next ;
@@ -96,45 +105,143 @@ class productkind
                 }
             }
         }
+        
+        void print ()
+        {
+            product * pro = this->head ;
+            
+            while ( pro != nullptr )
+            {
+                cout << pro->name << " " << pro->available << endl ;
+                pro = pro->next ;
+            }
+        }
 
 };
 
 
-
-class store
+class Store
 {
     private :
-        product * head ;
-        product * tail ;
-        int num ;
+        productkind * head ;
+        productkind * tail ;
+        int Knum ; //Number of productkinds .
     public:
-        store()
+        Store()
         {
             this->head = nullptr ;
             this->tail = nullptr ;
-            this->num = 0 ;
+            this->Knum = 0 ;
         }
-        ~store()
+        ~Store()
         {
-            product * pro = this->head ;
+            productkind * kind = this->head ;
 
-            while ( pro != nullptr )
+            while ( kind != nullptr )
             {
                 product * tmp = pro ;
-                pro = pro->getNext() ;
+                kind = kind->next ;
                 delete [] tmp ;
             }
 
             this->head = nullptr ;
             this->tail = nullptr ;
-            this->num = 0 ;
+            this->Knum = 0 ;
         }
 
+        productkind * addproductkind ( string kname )
+        {
+            productkind * kind = new productkind ( kname ) ;
+            kind = this->head ;
+            this->Knum ++ ;
+            
+            if ( kind == nullptr )
+            {
+                kind->prev = this->tail ;
+                kind->next = this->head ;
+                this->head = kind ;
+                this->tail = kind ;
+            }
+            else
+            {
+                productkind * tmp = this->head ;
+                
+                if ( kind->kname <= this->head->kname )
+                {
+                    kind->prev = this->head->prev ;
+                    kind->next = this->head ;
+                    this->head = kind ;
+                    return ;
+                }
+                if ( kind->kname >= this->tail->kname )
+                {
+                    kind->prev = this->tail ;
+                    kind->next = this->tail->next ;
+                    this->tail = kind ;
+                    return ;
+                }
+                if ( kind->kname <= tmp->next->kname )
+                {
+                    kind->prev = tmp ;
+                    kind->next = tmp->next ;
+                    tmp->next->prev = kind ;
+                    tmp->next = kind ;
+                    return ;
+                }
+            }
+        }
+        
+        void print ()
+        {
+            productkind *  kind = this->head ;
 
+            while ( kind != nullptr )
+            {
+                cout << kind->kname << endl << "Products of this kind is(are) : " << kind->pknum << endl ;
+                productkind::print() ;
+                kind = kind->next ;
+            }
+        }
 };
 
 int main( void )
 {
+    cout << "Welcome to my store." << endl ;
+
+    Store store ;
+    string kind , name ;
+    int num ;
+
+    //commands
+    {
+        string command ;
+        string add = "add" ;
+    }
+
+    while ( true )
+    {
+        cin >> command ;
+
+        switch ( command )
+        {
+            case "add" :
+            {
+                multimap<string,product> pro ;
+                cout << "What kind of product do you want to add? " ;
+                cin >> kind ;
+                cout << "What is the product name? " ;
+                cin >> name ;
+                cout << "How many of this product do you want to add? " ;
+                cin >> num ;
+
+                pro[kind] = Store.add_new_pro( name , num ) ;
+
+                cout << "product saved." << endl ;
+            }
+            break;
+        }
+
+    }
 
 
 
